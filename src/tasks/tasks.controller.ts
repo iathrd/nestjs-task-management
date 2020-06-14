48a2dced1ea1e95a69d0798a30783fcf29303f28
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-06-14 11:46:40 +0700

    CRUD

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, Patch } from '@nestjs/common';
 import { TasksService } from './tasks.service'
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -11,12 +12,27 @@ export class TasksController {
         return this.tasksService.getAllTask();
     }
 
-    @Post()
-    createTask(
-        @Body('title') title: string,
-        @Body('description') description: string
-    ): Task {
+    @Get(':id')
+    getTaskById(@Param('id') id: string): Task {
+        return this.tasksService.getTaskById(id);
+    }
 
-        return this.tasksService.createTask(title, description)
+    @Delete(':id')
+    deleteTaskById(@Param('id') id: string): void {
+        this.tasksService.deleteTaskById(id);
+    }
+
+    @Patch('/:id/status')
+    updateTaskStatus(
+        @Param('id') id: string,
+        @Body('status') status: TaskStatus
+    ): Task {
+        return this.tasksService.updateTaskStatus(id, status)
+    }
+
+    @Post()
+    createTask(@Body() createTaskDto: CreateTaskDto): Task {
+
+        return this.tasksService.createTask(createTaskDto)
     }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,7 +11,20 @@ export class TasksService {
         return this.tasks;
     }
 
-    createTask(title: string, description: string): Task {
+    getTaskById(id: string): Task {
+        return this.tasks.find(task => task.id === id);
+
+    }
+
+    deleteTaskById(id: string): void {
+        this.tasks = this.tasks.filter(task => task.id !== id);
+    }
+
+
+    createTask(createTaskDto: CreateTaskDto): Task {
+
+        const { title, description } = createTaskDto;
+
         const task: Task = {
             id: uuid(),
             title,
@@ -19,6 +33,12 @@ export class TasksService {
         };
 
         this.tasks.push(task)
+        return task;
+    }
+
+    updateTaskStatus(id: string, status: TaskStatus): Task {
+        const task = this.getTaskById(id);
+        task.status = status;
         return task;
     }
 }
